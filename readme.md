1f478b74a8dc3a3e89f0804ea3f743b304fa16fa
xhlife <13437593806@163.com>
2021-12-07 23:54:09 +0800

    vue3-grid-layout

---
+++ readme.md
@@ -0,0 +1,9 @@
+### 来源
+改自vue2版本的vue-grid-layout
+
+https://github.com/jbaysolutions/vue-grid-layout
+
+### 采用vue3
+
+#### 效果
+![alt 事件队列](/result.png)
--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 <div id="app">
   <div class="router">
     <router-link to="home">home</router-link>
-    <router-link to="about">about</router-link>
+    <!-- <router-link to="about">about</router-link> -->
   </div>
   <router-view></router-view>
 </div>
