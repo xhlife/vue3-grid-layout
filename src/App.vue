e8770beaef43fe88ec70d1cc0f7067df96a8c78d
xhlife <13437593806@163.com>
2022-12-11 17:34:34 +0800

    drga from out site

--- src/App.vue
+++ src/App.vue
@@ -7,41 +7,26 @@ import GridItem from "./components/Grid/GridItem.vue"
 
 let testLayout = ref(testData)
 
-// setTimeout(() => {
-//   testLayout.value.push({
-//     i: "1000",
-//     x: 2,
-//     y: 4,
-//     w: 6,
-//     h: 8,
-//     resizable: true,
-//     static: false,
-//     draggable: true
-//   })
-// }, 3000)
 const refLayout = ref()
-const $children: Array<any> = []
+
+const colNum = ref<number>(12)
+
 const mapCache: Map<string, any> = new Map()
-// setTimeout(() => {
-//   // console.log(refLayout.value.defaultGridItem.$el.style.display)
-//   console.log(mapCache)
-// }, 3000)
 
 function handleResize(i: string | number, w: number, h: number, x: number, y: number) {
   console.log(i, w, h, x, y)
 }
-const responsive = ref<boolean>(false)
+const responsive = ref<boolean>(true)
 
-// const setCache = new Set()
 function set$Children(vm: any) {
-  if (vm?.i) {
+  if (vm && vm.i) {
     mapCache.set(vm.i, vm)
   }
 }
 // {x: number | null; y: number | null}
 
 let mouseXY = {x: 0, y: 0}
-let DragPos = {x: 0, y: 0, w: 1, h: 1, i: 0}
+let DragPos = {x: 0, y: 0, w: 1, h: 1, i: ""}
 
 function drag(e: DragEvent) {
   e.stopPropagation()
@@ -49,19 +34,21 @@ function drag(e: DragEvent) {
   const t = document.getElementById("content") as HTMLElement
   let parentRect = t.getBoundingClientRect()
   let mouseInGrid = false
-  if (mouseXY.x > parentRect.left && mouseXY.y > parentRect.top) {
+  if (
+    mouseXY.x > parentRect.left &&
+    mouseXY.x < parentRect.right &&
+    mouseXY.y > parentRect.top &&
+    mouseXY.y < parentRect.bottom
+  ) {
     mouseInGrid = true
   }
   if (mouseInGrid === true && testLayout.value.findIndex(item => item.i === "drop") === -1) {
     testLayout.value.push({
-      x: (testLayout.value.length * 2) % 12,
-      y: testLayout.value.length + 12, // puts it at the bottom
+      x: (testLayout.value.length * 2) % colNum.value,
+      y: testLayout.value.length + colNum.value, // puts it at the bottom
       w: 3,
       h: 4,
-      i: "drop",
-      resizable: true,
-      draggable: true,
-      static: false
+      i: "drop"
     })
   }
 
@@ -72,25 +59,21 @@ function drag(e: DragEvent) {
       refLayout.value.defaultGridItem.$el.style.display = "none"
     } catch {}
     let el = mapCache.get("drop")
-    if (el) {
-      el.dragging = {top: mouseXY.y - parentRect.top, left: mouseXY.x - parentRect.left}
-      let new_pos = el.calcXY(mouseXY.y - parentRect.top, mouseXY.x - parentRect.left)
-      if (mouseInGrid === true) {
-        console.log(1)
+    if (!el) return
+    console.log("jjj")
 
-        refLayout.value.dragEvent("dragstart", "drop", new_pos.x, new_pos.y, 3, 4)
-        // DragPos.i = String(index)
-        // DragPos.x = testLayout.value[index].x
-        // DragPos.y = testLayout.value[index].y
-      }
-      // if (mouseInGrid === false) {
-      //   refLayout.value.dragEvent("dragend", "drop", new_pos.x, new_pos.y, 3, 4)
-      //   console.log("end")
-      //   testLayout.value = testLayout.value.filter(obj => obj.i !== "drop")
-      // }
+    el.dragging = {top: mouseXY.y - parentRect.top, left: mouseXY.x - parentRect.left}
+    let new_pos = el.calcXY(mouseXY.y - parentRect.top, mouseXY.x - parentRect.left)
+    if (mouseInGrid === true) {
+      refLayout.value.dragEvent("dragstart", "drop", new_pos.x, new_pos.y, 3, 4)
+      DragPos.i = String(index)
+      DragPos.x = testLayout.value[index].x
+      DragPos.y = testLayout.value[index].y
     }
-  } else {
-    mapCache.has("drop") && mapCache.delete("drop")
+    if (mouseInGrid === false) {
+      refLayout.value.dragEvent("dragend", "drop", new_pos.x, new_pos.y, 3, 4)
+      testLayout.value = testLayout.value.filter(obj => obj.i !== "drop")
+    }
   }
 }
 
@@ -106,25 +89,22 @@ function dragend() {
   ) {
     mouseInGrid = true
   }
+
   if (mouseInGrid === true) {
-    // alert(`Dropped element props:\n${JSON.stringify(DragPos, ["x", "y", "w", "h"], 2)}`)
-    refLayout.value.dragEvent("dragend", "drop", DragPos.x, DragPos.y, 1, 1)
+    refLayout.value.dragEvent("dragend", "drop", DragPos.x, DragPos.y, 3, 4)
     testLayout.value = testLayout.value.filter(obj => obj.i !== "drop")
     // UNCOMMENT below if you want to add a grid-item
-    /*
-                this.layout.push({
-                    x: DragPos.x,
-                    y: DragPos.y,
-                    w: 1,
-                    h: 1,
-                    i: DragPos.i,
-                });
-                this.$refs.gridLayout.dragEvent('dragend', DragPos.i, DragPos.x,DragPos.y,1,1);
-                try {
-                    this.$refs.gridLayout.$children[this.layout.length].$refs.item.style.display="block";
-                } catch {
-                }
-                */
+    nextTick(() => {
+      testLayout.value.push({
+        x: DragPos.x,
+        y: DragPos.y,
+        w: 3,
+        h: 4,
+        i: DragPos.i
+      })
+      refLayout.value.dragEvent("dragend", DragPos.i, DragPos.x, DragPos.y, 3, 4)
+      mapCache.delete("drop")
+    })
   }
 }
 
@@ -163,12 +143,13 @@ onMounted(() => {
           :ref="el => set$Children(el)"
           :key="item.i"
           class="test"
-          :static="item.static"
           :x="item.x"
           :y="item.y"
           :w="item.w"
           :h="item.h"
           :i="item.i"
+          :min-h="3"
+          :min-w="3"
           @resized="handleResize"
         >
           <!--<custom-drag-element :text="item.i"></custom-drag-element>-->
